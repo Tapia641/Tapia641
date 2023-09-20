f2b2884e49c74f02b75225f7a5e5a5f61016d18e
Luis Tapia <tapia641@gmail.com>
2023-09-20 04:48:09 -0600

    Adding the projects html page

---
+++ assets/dist/js/projects.js
@@ -0,0 +1,4 @@
+var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
+    interval: 2000, // Set the interval between slide transitions (in milliseconds)
+    pause: 'hover', // Pause on hover
+});
